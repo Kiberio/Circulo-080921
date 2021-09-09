564c048e2f2d71f6dbc3b94b8d4664ba1e9ae26a
Kiberio <dhiroshi3@gmail.com>
2021-09-08 21:50:19 -0300

    Criação de um novo Branch

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -3,27 +3,28 @@ using System;
 using System.Globalization;
 
 
-namespace ConsoleApp4
+namespace ConsoleApp5
 {
     class Program
     {
+      
         static void Main(string[] args)
         {
             //Declaração da classe cal
-            Cal calc = new Cal();
+            //Cal calc = new Cal();
 
             //Solicitando o valor do raio ao usuario
             Console.WriteLine("Entre com o valor do raio: ");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
             //Criando variaveis para carregar o valor das funções
-            double circ = calc.Circuferencia(raio);
-            double volume = calc.Volume(raio);
+            double circ = Cal.Circuferencia(raio);
+            double volume = Cal.Volume(raio);
 
             //Impressa dos resultados 
             Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Valor de PI: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Cal.Pi.ToString("F2", CultureInfo.InvariantCulture));
 
         }
 
--- ConsoleApp5/cal.cs
+++ ConsoleApp5/cal.cs
@@ -1,23 +1,20 @@
 ﻿//Daniel Hiroshi Fugikawa - 200209
-using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace ConsoleApp4
+namespace ConsoleApp5
 {
     //Classe para os calculos 
     class Cal
     {
         //Declaração da variavel d Pi
-        public double Pi = 3.14;
+          public static double Pi = 3.14;
 
-        public double Circuferencia(double r)
+          public static double Circuferencia(double r)
         {
             //Calculo da circunferencia
             return 2.0 * Pi * r;
         }
 
-        public double Volume(double r)
+          public static double Volume(double r)
         {
             //Calculo do volume
             return 4.0 / 3.0 * Pi * r * r * r;
