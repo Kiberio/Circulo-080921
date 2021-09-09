cf32803c4248a9222529b3c326be2683150b594f
Kiberio <dhiroshi3@gmail.com>
2021-09-08 21:18:17 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp5/cal.cs
@@ -0,0 +1,26 @@
+﻿//Daniel Hiroshi Fugikawa - 200209
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    //Classe para os calculos 
+    class Cal
+    {
+        //Declaração da variavel d Pi
+        public double Pi = 3.14;
+
+        public double Circuferencia(double r)
+        {
+            //Calculo da circunferencia
+            return 2.0 * Pi * r;
+        }
+
+        public double Volume(double r)
+        {
+            //Calculo do volume
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+    }
+}
